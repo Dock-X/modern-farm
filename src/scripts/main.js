86ab4f6c3e931e785ffa229c2464cca80536a2ce
William Dockery <williamdockery98@gmail.com>
2022-02-05 10:00:08 -0600

    Second commit

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -3,7 +3,16 @@ import { usePlant } from './field.js'
 
 import { createPlan } from './plan.js'
 const yearlyPlan = createPlan()
+console.log(yearlyPlan)
 
+const plan = [
+    ["Potato", "Soybean", "Soybean", "Corn"],
+    ["Wheat", "Corn", "Wheat", "Asparagus"],
+    ["Asparagus", "Wheat", "Soybean", "Corn"],
+    ["Asparagus", "Soybean", "Potato", "Wheat"]
+]
+plantSeeds(plan)
+//console.log(usePlant())
 
 import { createAsparagus } from "./seeds/asparagus.js"
 import { createCorn } from './seeds/corn.js'
@@ -11,13 +20,14 @@ import { createPotato } from './seeds/potato.js'
 import { createSoybean } from './seeds/soybean.js'
 import { createSunflower } from './seeds/sunflower.js'
 import { createWheat } from './seeds/wheat.js'
+import { plantSeeds } from './tractor.js'
 
-
-addPlant(createAsparagus())
-addPlant(createCorn())
-addPlant(createPotato())
-addPlant(createSoybean())
-addPlant(createSunflower())
-addPlant(createWheat())
+/*
+    addPlant(createAsparagus())
+    addPlant(createCorn())
+    addPlant(createPotato())
+    addPlant(createSoybean())
+    addPlant(createSunflower())
+    addPlant(createWheat())
+*/
 console.log(usePlant())
-//console.log(yearlyPlan())
